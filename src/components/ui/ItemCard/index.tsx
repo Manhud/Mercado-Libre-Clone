f0b833609b8f2728d6bb21fef80e8d6190c18135
Juan Rozo <rozoacwow@gmail.com>
2024-05-24 17:37:41 -0500

    chore: Update styles for ItemCard component

--- src/components/ui/ItemCard/index.tsx
+++ src/components/ui/ItemCard/index.tsx
@@ -2,9 +2,9 @@ import { Item } from "@/types/search";
 import styles from './ItemCard.module.scss';
 import Image from 'next/image';
 import Link from "next/link";
+
 interface ProductProps {
     item: Item;
-    
 }
 
 export const ItemCard: React.FC<ProductProps> = ({ item }) => {
@@ -12,27 +12,35 @@ export const ItemCard: React.FC<ProductProps> = ({ item }) => {
     <div className={styles['item-wrapper']}>
       <div className={styles['item-card']}>
         <figure className={styles['item-image']}>
-          <Image src={item.picture} alt={item.title} width={160} height={160}/>
+          <Image 
+            src={item.picture} 
+            alt={item.title} 
+            width={160} 
+            height={160} 
+            className={styles['item-image__img']}
+          />
         </figure>
         <div className={styles['item-content-wrapper']}>
           <Link href={`/items/${item.id}`} className={styles['item-content-wrapper--title']}>
-            {item.title}
+              {item.title}
           </Link>
           <div className={styles['item-content-wrapper__columns']}>
             <div className={styles['item-content-wrapper__columns--left']}>
               <div className={styles['item-group']}>
-                <span className={styles['item-group--amount']}>{item.price.amount.toLocaleString("es-AR", {
-                  currency: item.price.currency,
-                  style: 'currency',
-                })}</span>
+                <span className={styles['item-group--amount']}>
+                  {item.price.amount.toLocaleString("es-AR", {
+                    currency: item.price.currency,
+                    style: 'currency',
+                  })}
+                </span>
               </div>
             </div>
             <div className={styles['item-content-wrapper__columns--right']}>
-
+              
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
