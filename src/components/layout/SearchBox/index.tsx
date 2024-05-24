a51e328c2a98ef2fc56101b516cd856376d40086
Juan Rozo <rozoacwow@gmail.com>
2024-05-23 20:08:26 -0500

    chore: Update dependencies and add new files for colors and spacings

--- src/components/layout/SearchBox/index.tsx
+++ src/components/layout/SearchBox/index.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link'
 import styles from './SearchBox.module.scss'
+import Image from 'next/image'
 
 export const SearchBox = () => {
   return (
-    <p className={styles.title}>SearchBox</p>
+    <header className={styles['nav-header']}>
+      <div className={styles['nav-header__nav-bounds']}>
+          <Link href='/' className={styles['nav-header__logo']}>
+            <Image
+              src="/images/logo.png"
+              width={134}
+              height={34}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+    </header>
   )
 }
