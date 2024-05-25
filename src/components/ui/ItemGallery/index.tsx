50de4e1fc2c1e312b638e9400462baec2d115904
Juan Rozo <rozoacwow@gmail.com>
2024-05-25 12:39:49 -0500

    refactor: Update import statement for Item type in ItemGallery component

--- src/components/ui/ItemGallery/index.tsx
+++ src/components/ui/ItemGallery/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import styles from './ItemGallery.module.scss'
-import { Item } from '@/types/search';
+import { Item } from '@/types/item';
 
 interface ItemGalleryProps {
     item: Item;
